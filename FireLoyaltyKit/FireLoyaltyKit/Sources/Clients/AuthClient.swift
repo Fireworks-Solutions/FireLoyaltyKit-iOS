24b5b8d9d572956bdb579b5a4602e17d9de9a0f6
Taufiq <taufiqgiri.math@gmail.com>
2026-04-14 22:32:40 +0700

    feat: added property parent_custid in LoginResponse model

--- FireLoyaltyKit/FireLoyaltyKit/Sources/Clients/AuthClient.swift
+++ FireLoyaltyKit/FireLoyaltyKit/Sources/Clients/AuthClient.swift
@@ -530,6 +530,7 @@ public struct LoginResponse: Decodable, Identifiable {
     public let url: String?
     public let token: String?
     public let reset: String?
+    public let parent_custid: String?
 
     private enum CodingKeys: String, CodingKey {
         case status, message, custid, name, fname
@@ -541,6 +542,7 @@ public struct LoginResponse: Decodable, Identifiable {
         case url
         case token
         case reset
+        case parent_custid
     }
 
     // If you want a default value for phone when the key is missing or null:
@@ -559,6 +561,7 @@ public struct LoginResponse: Decodable, Identifiable {
         url            = try container.decodeIfPresent(String.self, forKey: .url)
         token          = try container.decodeIfPresent(String.self, forKey: .token)
         reset          = try container.decodeIfPresent(String.self, forKey: .reset)
+        parent_custid  = try container.decodeIfPresent(String.self, forKey: .parent_custid)
     }
 }
 
