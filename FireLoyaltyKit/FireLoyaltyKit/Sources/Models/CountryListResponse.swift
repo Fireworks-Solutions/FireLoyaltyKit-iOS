5d67cb1ad2017ffa5401dfaf04b9c69429861d43
Mani <mani@fireworks.my>
2026-06-04 11:06:37 +0800

    Country List Model updated.

--- FireLoyaltyKit/FireLoyaltyKit/Sources/Models/CountryListResponse.swift
+++ FireLoyaltyKit/FireLoyaltyKit/Sources/Models/CountryListResponse.swift
@@ -11,7 +11,7 @@ public struct CountryListResponse: Codable {
     public let status: String?
     public let message: String?
     public let results: [StatesListModel]?
-    public let result: [StatesListModel]?
+    public let result: [CountryListModel]?
 }
 
 public struct CountryListModel: Codable, Identifiable {
